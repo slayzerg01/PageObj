a0226df806fe36a9767b8b3030df9f2f79e6d5e2
Nitro <bagdat250716@gmail.com>
2023-01-07 21:57:33 +0500

    Параметризированный тест с несколькими url

--- test_product_page.py
+++ test_product_page.py
@@ -1,16 +1,27 @@
 from selenium.webdriver.common.by import By
+import pytest
 from .pages.main_page import MainPage
 from .pages.product_page import ProductPage
 import time
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+links = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
 
-def test_guest_can_add_product_to_basket(browser):
+@pytest.mark.parametrize('link', links)
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_product_in_basket()
     #page.solve_quiz_and_get_code()
-    time.sleep(70)
+    #time.sleep(70)
     
     
     
