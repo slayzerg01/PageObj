522023af39a4809625e8b6acc15cd146d900a2f3
Nitro <bagdat250716@gmail.com>
2023-01-08 18:42:10 +0500

    Методы логина из main_page перенесены в base_page

--- test_product_page.py
+++ test_product_page.py
@@ -15,32 +15,44 @@ links = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?pr
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
 
-@pytest.mark.parametrize('link', links)
-def test_guest_can_add_product_to_basket(browser, link):
+# @pytest.mark.parametrize('link', links)
+# def test_guest_can_add_product_to_basket(browser, link):
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.should_be_add_product_in_basket()
+#     page.solve_quiz_and_get_code()
+#     page.compare_name_and_price()
+
+# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+#     page = ProductPage(browser, links[0])
+#     page.open()
+#     page.should_be_add_product_in_basket()
+#     page.solve_quiz_and_get_code()
+#     page.should_not_be_success_message()
+
+# def test_guest_cant_see_success_message(browser):
+#     page = ProductPage(browser, links[0])
+#     page.open()
+#     page.should_not_be_success_message()
+
+# def test_message_disappeared_after_adding_product_to_basket(browser):
+#     page = ProductPage(browser, links[0])
+#     page.open()
+#     page.should_be_add_product_in_basket()
+#     page.solve_quiz_and_get_code()
+#     page.should_dissapear_of_success_message()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
-    page.should_be_add_product_in_basket()
-    page.solve_quiz_and_get_code()
-    page.compare_name_and_price()
+    page.should_be_login_link()
 
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    page = ProductPage(browser, links[0])
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
     page.open()
-    page.should_be_add_product_in_basket()
-    page.solve_quiz_and_get_code()
-    page.should_not_be_success_message()
-
-def test_guest_cant_see_success_message(browser):
-    page = ProductPage(browser, links[0])
-    page.open()
-    page.should_not_be_success_message()
-
-def test_message_disappeared_after_adding_product_to_basket(browser):
-    page = ProductPage(browser, links[0])
-    page.open()
-    page.should_be_add_product_in_basket()
-    page.solve_quiz_and_get_code()
-    page.should_dissapear_of_success_message()
+    page.go_to_login_page()
 
     
 
